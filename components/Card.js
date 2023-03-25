bb0981450decf850bc6d362ab91d2282f2241984
Jenny <jennyngu@hawaii.edu>
2023-03-24 14:03:39 -1000

    Update packages and fix Link errors

--- components/Card.js
+++ components/Card.js
@@ -3,14 +3,12 @@ import Link from "next/link";
 const Card = ({ name, email, id }) => {
   return (
     <div className="tc bg-light-green dib br3 pa3 ma3 grow bw2 shadow-5">
-      <Link prefetch href={`/robots?id=${id}`} as={`/robots/${id}`}>
-        <a className="no-underline">
+      <Link prefetch href={`/robots?id=${id}`} as={`/robots/${id}`} className="no-underline">
           <img src={`https://robohash.org/${id}?size=200x200`} alt="robots" />
           <div>
             <h2>{name}</h2>
             <p>{email}</p>
           </div>
-        </a>
       </Link>
     </div>
   );
